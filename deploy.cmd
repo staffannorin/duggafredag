8d079af7b6835d504a70495c522c076a72ae41de
Staffan Norin <staffan.norin@outlook.com>
2016-12-06 20:42:44 +0100

    asdfa

--- deploy.cmd
+++ deploy.cmd
@@ -74,8 +74,8 @@ pushd "%DEPLOYMENT_TARGET%"
 :: 2. Create virtual environment
  IF NOT EXIST "%DEPLOYMENT_TARGET%\env" (
    echo Creating %PYTHON_RUNTIME% virtual environment.
-   D:\home\Python27\python.exe -m pip install virtualenv
-   D:\home\Python27\python.exe -m virtualenv env
+   D:\Python27\python.exe -m pip install virtualenv
+   D:\Python27\python.exe -m virtualenv env
    IF !ERRORLEVEL! NEQ 0 goto error
  ) ELSE (
    echo Found compatible virtual environment.
